d5c26b3482c94d8cc4414c61d979f2cb98b58544
Ahmed Basuony <A.Basuony.social@gmail.com>
2025-10-22 01:29:55 +0300

    upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">مرحبا بالجميع </h1>
+      <button className="btn btn-success mt-3">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/NavbarLogin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavbarLogin = () => {
+  return <div>NavbarLogin</div>;
+};
+
+export default NavbarLogin;
